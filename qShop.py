d1f25f0d29499fc99f8a1b50b57e2b20f90396c3
Tom <tom.klein@smail.h-brs.de>
2019-12-10 17:26:36 +0100

    fast fertig aber oller bug

--- qShop.py
+++ qShop.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import json
 import numpy as np
+from copy import deepcopy
+import seaborn as sns
 
 
 class myProblem:
-    with open(r'C:\Users\Tom\Desktop\example.json') as f:
+    with open(r'C:\Users\Tom\Desktop\Q-Learn\example.json') as f:
         data = json.load(f)
     name = data["name"]
     kommentar = data["kommentar"]
-    reihenfolge = data["reihenfolge"]
-    bearbeitungszeiten = data["bearbeitungszeiten"]
+    reihenfolge = data["reihenfolge"] #[Maschine][Job]
+    bearbeitungszeiten = data["bearbeitungszeiten"] #[Maschine][Job]
     zielfunktion = data["zielfunktion"]
     if(zielfunktion == "Lmax"):
         liefertermin = data["liefertermin"]
@@ -24,33 +26,50 @@ p1 = myProblem()
 #-----------------------
 
 #für den aktuellen State
-time = 0
-maxTime = np.matrix.sum(np.matrix(p1.bearbeitungszeiten))
-belegung = np.zeros((p1.m, p1.n))       #null = Maschine frei, nicht null = Zeit bis Maschine frei
-zuBearbeiten = p1.bearbeitungszeiten    #null = ist/wird bearbeitet, nicht null muss bearbeitet werden
-nextMaschine = p1.reihenfolge[0]        #nextMaschine[i] gibt nächste Machine für Aufrtrag i. Falls -1 ist, ist Aufrag i fertig
-maxRandQValue = 10                      #Q Table zufällig zwischen 0 und 10
-orderPointer = np.zeros(p1.m)           #Zeigt auf die Stelle in der Q tabelle der Machinen des index, der als nächsts dran ist
-q = np.array(p1.m)
-for i in range(0, p1.m):
-        q[i] = np.randint(high=maxRandQValue,size=(p1.n, p1.n))
 
-bestValue = -1
-bestConfig = np.zeros((p1.m, maxTime))   #Gant Diagramm
-currentConfig = np.zeros((p1.m, maxTime))
+maxTime = np.matrix.sum(np.matrix(p1.bearbeitungszeiten))
+belegung = np.zeros((p1.m, p1.n), dtype=int)       #null = Maschine frei, nicht null = Zeit bis Maschine frei
+zuBearbeiten = deepcopy(p1.bearbeitungszeiten)    #null = ist bearbeitet, nicht null muss bearbeitet werden
+nextMachinePointer = np.zeros((p1.n), dtype=int)   #Pointer auf den aktuellen Wert in der Reihenfolge Matrix
+nextMaschine = deepcopy(p1.reihenfolge[0])        #nextMaschine[i] gibt nächste Machine für Aufrtrag i. Falls -1 ist, ist Aufrag i fertig
+working = []                            #Liste aller aktuell laufenden Prozesse (Auftrag, Maschine Tupel)
+maxRandQValue = 10                      #Q Table zufällig zwischen 0 und 10
+orderPointer = np.zeros(p1.m, dtype=int)           #Zeigt auf die Stelle in der Q tabelle der Machinen des index, der als nächsts dran ist
+q = np.random.randint(low=0, high=maxRandQValue, size=(p1.m, p1.n, p1.n))    #[Maschine][orderPointer][Job]
+countAlpha = np.zeros((p1.m, p1.n, p1.n), dtype=int)
+bestTime = maxTime
+bestConfig = np.zeros((p1.m, maxTime), dtype=int)   #Gant Diagramm
+currentConfig = np.zeros((p1.m, maxTime), dtype=int)
 eps = 0.3
-idleProb = 0.05   
+idleProb = 0.05
+gamma = 0.7
+curTime = 0
 
 def learn():
-    while(not np.all(nextMaschine[i] == -1 for i in range(0,p1.n))):
+    while(np.matrix.sum(np.matrix(zuBearbeiten)) != 0):
         options = getCurrentOptions()
         idle = np.random.random_sample()
-        while(len(options) > 0 & idle > idleProb):
-            rand = np.random.choise(options)
+        while(len(options) > 0 and idle > idleProb):
+            randNum = np.random.randint(low=0, high=len(options), size=1)[0]
+            rand = options[randNum]
             best = getMaxQValue(options)
-            choose = np.random.choise([rand, best], 1, p=[eps, 1-eps])
-            schedule(options[choose])
+            chooseNum = np.random.choice(a=[0, 1], size=1, p=[eps, 1-eps])
+            if chooseNum == 1:
+                choose = rand
+            else:
+                choose = best
+            updateQ(choose)
+            working.append(choose)
+            i = choose[0]
+            j = choose[1]
+            belegung[j][i] = p1.bearbeitungszeiten[j][i]
+            orderPointer[j] += 1
             options = getCurrentOptions()
+        updateAll()
+    updateAll()
+    resetAll()
+
+
         
 
 def getMaxQValue(options):
@@ -59,11 +78,11 @@ def getMaxQValue(options):
     for o in options:
         i = o[0]
         j = o[1]
-        mTable = q[j]
-        cur = mTable[orderPointer[j]][i] #Q wert zu tupel (i,j)=o
-        if(cur > bestValue):
-            bestO = o
-            bestValue = cur
+        if(orderPointer[j] != p1.n):
+            cur = q[j][orderPointer[j]][i] #Q wert zu tupel (i,j)=o
+            if(cur > bestValue):
+                bestO = o
+                bestValue = cur
     return bestO
 
 
@@ -75,23 +94,79 @@ def getCurrentOptions():
         j = nextMaschine[i]
         if(j != -1):
             if(belegung[j][i] == 0):
-                result.append((i+1,j))
+                result.append((i,j))
     return result #tupel (auftrag, maschine)
+    
 
-def schedule(t):
+def updateAll():
+    global curTime, bestConfig, bestTime, working, zuBearbeiten
+
+    print(working)
+    print(zuBearbeiten)
+    print(belegung)
+    if len(working) == 0 and np.matrix.sum(np.matrix(zuBearbeiten)) == 0:
+        print("fertig")
+        if(curTime < bestTime):
+            bestTime = curTime
+            bestConfig = currentConfig
+    for t in working:
+        i = t[0]
+        j = t[1]
+        currentConfig[j][curTime] = i+1
+        belegung[j][i] -= 1
+        zuBearbeiten[j][i] -= 1
+
+    for t in working:
+        i = t[0]
+        j = t[1]
+        if belegung[j][i] == 0:     #fertig geworden
+            working.remove(t)
+            nextMachinePointer[i] +=1
+            if(nextMachinePointer[i] == p1.m):
+                nextMaschine[i] = -1
+            else:
+                nextMaschine[i] = p1.reihenfolge[nextMachinePointer[i]][i]
+        
+    curTime += 1
+
+def resetAll():
+    global belegung, zuBearbeiten, nextMachinePointer, nextMaschine, working, orderPointer, countAlpha, currentConfig, curTime
+    curTime = 0
+    belegung = np.zeros((p1.m, p1.n), dtype=int)
+    zuBearbeiten = deepcopy(p1.bearbeitungszeiten)
+    nextMachinePointer = np.zeros((p1.n), dtype=int)
+    nextMaschine = deepcopy(p1.reihenfolge[0])
+    working = []
+    orderPointer = np.zeros(p1.m, dtype=int)
+    countAlpha = np.zeros((p1.m, p1.n, p1.n), dtype=int)
+    currentConfig = np.zeros((p1.m, maxTime), dtype=int)
+
+
+
+def updateQ(t):
     i = t[0]
     j = t[1]
-    #update all
+    curPos = orderPointer[j]
+    countAlpha[j][curPos][i] += 1
+    alphaValue = 1/(1+countAlpha[j][curPos][i])
+
+    if(np.all(zuBearbeiten[j][p] == 0 for p in range(0, p1.n) if p not in [i])): #wenn i der letzte Job auf j ist, dann belohnung = fertigstellungszeitpunkt von i, also damit Cmax fuer Maschine j
+        r = curTime + p1.bearbeitungszeiten[j][i]
+    else:
+        r = 0
+    qMin = np.min([q[j][p][i] for p in range(0, p1.n)])
+    q[j][curPos][i] = (1-alphaValue) * q[j][curPos][i] + alphaValue * (r + gamma * qMin)
 
 
 
+print(q)
+
+for i in range(1):
+    learn()
 
 
-#for i in range(100):
-#    learn()
-
-#print(getCurrentOptions())
-
-#print(bestConfig)
+print(bestConfig)
+print(q)
+ax = sns.heatmap(bestConfig)
 
 
